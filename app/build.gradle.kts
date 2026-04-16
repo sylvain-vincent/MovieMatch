bd4d28235d1f248159f0f0daaf3e79c7e5a2e923
sylvain-vincent <sylvainvincentdev@gmail.com>
2026-04-16 13:30:36 +0200

    build(deps): add Hilt (DI)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -40,6 +42,8 @@ android {
 }
 
 dependencies {
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -24,4 +24,3 @@ dependencyResolutionManagement {
 
 rootProject.name = "MovieMatch"
 include(":app")
- 
