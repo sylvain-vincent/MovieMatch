fa9d00f48042501ef3b672501887f541d3355091
sylvain-vincent <sylvainvincentdev@gmail.com>
2026-04-29 13:35:17 +0200

    build(deps): add Coil library

--- feature/match/build.gradle.kts
+++ feature/match/build.gradle.kts
@@ -28,5 +28,6 @@ dependencies {
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.material3)
     implementation(libs.hilt.android)
+    implementation(libs.coil.compose)
     ksp(libs.hilt.compiler)
 }
